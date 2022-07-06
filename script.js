2449660c0ca9d8cdd1da3c4e515afe21e709354d
Sudeeeep <sudeepn15@gmail.com>
2022-07-06 16:09:44 +0530

    feat: Add renderToDisplay funtion with manual input
    This allows to fill boxes with X & O with manually created player
    objects

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 //Game Board Module which returns gameBoard Array
 const gameBoardModule = (function () {
-    let gameBoard = [];
-
+    let gameBoard = ["", "", "", "", "", "", "", "", ""];
     return {
         gameBoard
     };
@@ -10,29 +9,64 @@ const gameBoardModule = (function () {
 
 //Display Controller Module
 const displayControllerModule = (function () {
-    let testDisp = () => {
-        console.log("IT WORKS!!")
+    const gridBox = document.querySelectorAll(".grid-box");
+    gridBox.forEach(grid => {
+        grid.addEventListener("click", renderToDisplay);
+    })
+
+    //Function to render X and O on display
+    function renderToDisplay(e) {
+        let index = e.target.dataset.index;
+
+        // Restrict players from playing in spots that are already taken 
+        if (gameBoardModule.gameBoard[index] == "") {
+
+            //Check if it is Player1's turn to play
+            if (player1.isPlayerActive == "true") {
+
+                gameBoardModule.gameBoard[index] = player1.playerSymbol;
+                e.target.innerText = gameBoardModule.gameBoard[index];
+                console.log(gameBoardModule.gameBoard);
+                player1.isPlayerActive = "false";
+                player2.isPlayerActive = "true";
+
+            }
+            //Check if it is Player2's turn to play
+            else if (player2.isPlayerActive == "true") {
+
+                gameBoardModule.gameBoard[index] = player2.playerSymbol;
+                e.target.innerText = gameBoardModule.gameBoard[index];
+                console.log(gameBoardModule.gameBoard);
+                player2.isPlayerActive = "false";
+                player1.isPlayerActive = "true";
+            }
+        }
+
+
     };
-    return {
-        testDisp
-    };
+
+    // return {
+    //     renderToDisplay
+    // };
 })();
 
 
 //Factory Function to create players
 function createPlayer(playerName, playerNumber, playerSymbol) {
-    const getPlayer = function () {
-        console.log(`Player Name:${playerName} and Player Number:${playerNumber}`)
+    let isPlayerActive = "";
+    if (playerNumber == "1") {
+        isPlayerActive = "true";
+    } else {
+        isPlayerActive = "false";
     }
     return {
         playerName,
         playerNumber,
-        getPlayer,
         playerSymbol,
+        isPlayerActive,
     }
 }
 
 
-
 let player1 = createPlayer("Sudeep", "1", "X");
-let player2 = createPlayer("Nair", "1", "O");
+let player2 = createPlayer("Nair", "2", "O");
