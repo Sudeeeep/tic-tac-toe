40ed9280f700919ef04963ebef288a921e5b6e5f
Sudeeeep <sudeepn15@gmail.com>
2022-07-07 16:20:27 +0530

    feat: add new UI player details
    This allows user enter player1 and player2 details and choose symbols 'X' or 'O'

--- script.js
+++ script.js
@@ -7,6 +7,37 @@ const gameBoardModule = (function () {
 })();
 
 
+//Module for selecting elements from DOM
+const selectElements = (function () {
+
+    const form = document.querySelector("form");
+    const playerOneName = document.querySelector('input[name="player-one-name"]');
+    const playerTwoName = document.querySelector('input[name="player-two-name"]')
+    const startGameBtn = document.querySelector(".start-game");
+    const playerOneRadioBtn = document.querySelectorAll('input[name="player-one-choice"]')
+    const playerTwoRadioBtn = document.querySelectorAll('input[name="player-two-choice"]')
+    const emptyWaring = document.getElementById("empty-warning");
+    const symbolWaring = document.getElementById("symbol-warning");
+    const gridContainer = document.querySelector(".grid-container");
+    const playerDetailsScreen = document.querySelector(".player-details-screen");
+    const mainHeader = document.querySelector(".main-header");
+
+    return {
+        form,
+        playerOneName,
+        playerTwoName,
+        startGameBtn,
+        playerOneRadioBtn,
+        playerTwoRadioBtn,
+        emptyWaring,
+        symbolWaring,
+        gridContainer,
+        playerDetailsScreen,
+        mainHeader,
+    };
+})();
+
+
 //Display Controller Module
 const displayControllerModule = (function () {
     const gridBox = document.querySelectorAll(".grid-box");
@@ -53,12 +84,42 @@ const displayControllerModule = (function () {
 
 //Factory Function to create players
 function createPlayer(playerName, playerNumber, playerSymbol) {
-    let isPlayerActive = "";
-    if (playerNumber == "1") {
-        isPlayerActive = "true";
-    } else {
-        isPlayerActive = "false";
+
+    selectElements.startGameBtn.addEventListener("click", validateChoice)
+
+    // Form Validation   
+    function validateChoice(e) {
+
+        if (selectElements.playerOneName.value == "" || selectElements.playerTwoName.value == "") {
+
+            selectElements.emptyWaring.style.display = "block"
+
+        } else if (selectElements.playerOneRadioBtn[0].checked == selectElements.playerTwoRadioBtn[0].checked ||
+            selectElements.playerOneRadioBtn[1].checked == selectElements.playerTwoRadioBtn[1].checked) {
+            console.log("ERROR!");
+            selectElements.symbolWaring.style.display = "block"
+            selectElements.emptyWaring.style.display = "none"
+
+        } else {
+            selectElements.mainHeader.style.display = "block";
+            selectElements.gridContainer.style.display = "grid";
+            selectElements.playerDetailsScreen.style.display = "none"
+        }
     }
+
+
+    let isPlayerActive = "";
+    makePlayerActive();
+
+    // Make Player1 Active so Player1 can play first
+    function makePlayerActive() {
+        if (playerNumber == "1") {
+            isPlayerActive = "true";
+        } else {
+            isPlayerActive = "false";
+        }
+    }
+
     return {
         playerName,
         playerNumber,
@@ -70,3 +131,6 @@ function createPlayer(playerName, playerNumber, playerSymbol) {
 
 let player1 = createPlayer("Sudeep", "1", "X");
 let player2 = createPlayer("Nair", "2", "O");
+
+// TODO: THIS WILL HELP WITH EXTRACTING PlAYER CHOICE VALUE
+// playerOneRadioBtn[0].labels[0].innerText
